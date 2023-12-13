090e7cce343594fcf546856b03ec2e10694a4b36
Ellen Sandqvist <ell1sandqvist@gmail.com>
2023-12-13 17:00:23 +0100

    added a new file with the functionality to show date and time

---
+++ js/date.js
@@ -0,0 +1,31 @@
+//eventlistener with a function that starts when window is loaded
+window.addEventListener('load', showCurrentTime);
+
+function showCurrentTime(){
+    const dateDisplay = document.querySelector('.date');
+
+    //get current date and time from the Date object
+    const currentDate = new Date();
+
+    //get current time
+    const time = currentDate.getHours() + ":" + currentDate.getMinutes();
+
+    //get date of the month
+    const dateOfMonth = currentDate.getDate();
+
+    //array of months needed to convert the "month-number" from the Date object
+    const months = [
+        "Januari","Februari","Mars",
+        "April","Maj","Juni",
+        "Juli","Augusti","September",
+        "Oktober","November","December"
+    ];
+    /* date.getMonth returns a number 0-11, months[...] are used to convert this to the actual month */
+    let month = months[currentDate.getMonth()];
+
+    //get current year
+    let year = currentDate.getFullYear();
+
+    dateDisplay.innerHTML = `<p><strong class="date-span">${time}</strong> ${dateOfMonth} ${month} ${year}</p>`;
+    this.setTimeout(()=>{showCurrentTime()}, 20_000);
+};
