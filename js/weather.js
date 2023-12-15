7723b0c7fe76b60ac7d040c78830345597885bbd
Ellen Sandqvist <ell1sandqvist@gmail.com>
2023-12-15 15:02:04 +0100

    added a new js file for the weather function and wrote some todo notes

---
+++ js/weather.js
@@ -0,0 +1,11 @@
+// todo use browser geolocation api to get users location
+//todo fetch weatherData for user location
+// ev. find out how to handle passing midnight 
+//todo render relevant weather data on page
+    // 1. if-statement to check what img to display and ev. belonging text
+    // 2. display date/day
+    // 3. display temperature
+    // 4. ev display wind speed, gust and direction 
+        // 4a. Use if-statement to chose right wind arraw depending on wind degree
+// todo use date for timedisplay and listen for change - on change fetch new weatherData
+// todo modal with info about weather forecast i.e. tomorrows weather is the forecast for the weather in 24h...
