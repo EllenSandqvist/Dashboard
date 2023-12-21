32f103b307c062df2af8365202520d40bae0a3cf
Ellen Sandqvist <ell1sandqvist@gmail.com>
2023-12-21 14:43:42 +0100

    continued working on adding and removing links. Also added validation for adding links form

--- js/links.js
+++ js/links.js
@@ -10,49 +10,60 @@ renderLinks();
 
 //function renderLinks
 function renderLinks(){
-    if(linkArray.length !== 0){
-        const linkContainer = document.querySelector('.link-container');
+    const linkContainer = document.querySelector('.link-container');
     
         //user linkArray.map to render links
         //vi vill lägga in produkterna med pris < 200 på vår hemsida i HTML. Vi skapar flera html element och sparar dessa i mapResults
-    //smart att lägga in klasser på det här viset
-    let mapLinks = linkArray.map((link, index) => { 
-        return `
-            <div class="link-div">
-                <a class="links" href="${link.linkUrl}" target="_blank"><h3 class="link-h3">${link.linkName}</h3></a>
-                <i onclick= "deleteLink(${index})" class="fa-regular fa-trash-can"></i>
-            </div>
-        `
-    });
-
+        //smart att lägga in klasser på det här viset
+        let mapLinks = linkArray.map((link, index) => { 
+            return `
+                <div class="link-div">
+                    <a class="links" href="${link.linkUrl}" target="_blank"><h3 class="link-h3">${link.linkName}</h3></a>
+                    <i onclick= "deleteLink(${index})" class="fa-regular fa-trash-can"></i>
+                </div>
+            `
+        });
     linkContainer.innerHTML = mapLinks.join('');
-    }
 }
 
 //function to saveLinks in array in localStorage
-//todo Add function to check required fields
 saveLinkBtn.addEventListener('click', function(){
-    linkObj = {
-        linkName: document.getElementById('link-name').value, 
-        linkUrl: document.getElementById('link-url').value 
+    const modalForm = document.querySelector('form');
+    
+    if(modalForm.checkValidity()) {
+        console.log("formuläret är rätt ifyllt!");
+        linkObj = {
+            linkName: document.getElementById('link-name').value, 
+            linkUrl: document.getElementById('link-url').value 
+            }
+        
+        linkArray.push(linkObj);
+        localStorage.setItem('links', JSON.stringify(linkArray));
+
+    } else {
+        alert("Alla fält är inte korrekt ifyllda. \nKom du ihåg https:// i början av webbadressen? \nProva igen!");
     }
-    
-    linkArray.push(linkObj);
-    
-    localStorage.setItem('links', JSON.stringify(linkArray));
 })
 
-
+//------- Show add-links modal -------------
 addLinkBtn.addEventListener('click', function(){
     linkModal.classList.remove('link-modal-hidden');
 });
 
+//------- Hide add-links modal -------------
 closeLinkModal.addEventListener('click', function(){
     linkModal.classList.add('link-modal-hidden');
 })
 
+//------- Delete links -------------------------
 console.log(linkArray);
 function deleteLink(linkIndex) {
     linkArray.splice(linkIndex, 1);
+
+    if(linkArray.length === 0) {
+        localStorage.removeItem('links');
+    } else {
+        localStorage.setItem('links', JSON.stringify(linkArray));
+    }
     renderLinks();
 }
