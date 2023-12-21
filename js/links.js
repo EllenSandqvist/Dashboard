9fb2c6048ef0c2d97f1f7ad7306a7fab1830137d
Ellen Sandqvist <ell1sandqvist@gmail.com>
2023-12-21 12:06:52 +0100

    started working on function to add and remove links

--- js/links.js
+++ js/links.js
@@ -10,23 +10,27 @@ renderLinks();
 
 //function renderLinks
 function renderLinks(){
-    const linkContainer = document.querySelector('.link-container');
+    if(linkArray.length !== 0){
+        const linkContainer = document.querySelector('.link-container');
     
         //user linkArray.map to render links
         //vi vill lägga in produkterna med pris < 200 på vår hemsida i HTML. Vi skapar flera html element och sparar dessa i mapResults
     //smart att lägga in klasser på det här viset
-    let mapLinks = linkArray.map((link) => { 
+    let mapLinks = linkArray.map((link, index) => { 
         return `
             <div class="link-div">
                 <a class="links" href="${link.linkUrl}" target="_blank"><h3 class="link-h3">${link.linkName}</h3></a>
+                <i onclick= "deleteLink(${index})" class="fa-regular fa-trash-can"></i>
             </div>
         `
     });
 
     linkContainer.innerHTML = mapLinks.join('');
+    }
 }
 
 //function to saveLinks in array in localStorage
+//todo Add function to check required fields
 saveLinkBtn.addEventListener('click', function(){
     linkObj = {
         linkName: document.getElementById('link-name').value, 
@@ -46,3 +50,9 @@ addLinkBtn.addEventListener('click', function(){
 closeLinkModal.addEventListener('click', function(){
     linkModal.classList.add('link-modal-hidden');
 })
+
+console.log(linkArray);
+function deleteLink(linkIndex) {
+    linkArray.splice(linkIndex, 1);
+    renderLinks();
+}
