14329be1002b1e633d6dcc6178e4b59cafa23729
Ellen Sandqvist <ell1sandqvist@gmail.com>
2023-12-21 16:37:02 +0100

    improved comments and structure

--- js/links.js
+++ js/links.js
@@ -1,69 +1,88 @@
+//variables for needed HTML-elements
 const addLinkBtn = document.getElementById('add-link-button');
 const linkModal = document.getElementById('link-modal');
 const closeLinkModal = document.getElementById('link-modal-close');
 const saveLinkBtn = document.getElementById('save-link-button');
 
-let linkArray = JSON.parse(localStorage.getItem('links')) || [];
+/* if there is a linkArray in localStorage convert it to js and store in linkArray,
+otherwise store an empty array */
+const linkArray = JSON.parse(localStorage.getItem('links')) || [];
 
 //function call to render links
 renderLinks();
 
-//function renderLinks
+//------- FUNCTION renderLinks -------------
 function renderLinks(){
     const linkContainer = document.querySelector('.link-container');
-    
-        //user linkArray.map to render links
-        //vi vill lägga in produkterna med pris < 200 på vår hemsida i HTML. Vi skapar flera html element och sparar dessa i mapResults
-        //smart att lägga in klasser på det här viset
+
+        //use map() to generate html element for the dashboard
+        //add index to deleteLink function call to easily find right link to remove
         let mapLinks = linkArray.map((link, index) => { 
             return `
                 <div class="link-div">
-                    <a class="links" href="${link.linkUrl}" target="_blank"><h3 class="link-h3">${link.linkName}</h3></a>
+                    <a class="links" href="${link.linkUrl}" target="_blank">
+                        <h3 class="link-h3">${link.linkName}</h3>
+                    </a>
                     <i onclick= "deleteLink(${index})" class="fa-regular fa-trash-can"></i>
                 </div>
             `
         });
+    //render html elements from mapLinks and use join to remove ","
     linkContainer.innerHTML = mapLinks.join('');
 }
 
-//function to saveLinks in array in localStorage
-saveLinkBtn.addEventListener('click', function(){
+//------- FUNCTION to save links -------------
+saveLinkBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
     const modalForm = document.querySelector('form');
     
+    //if statement to check so form is correct filled
     if(modalForm.checkValidity()) {
         console.log("formuläret är rätt ifyllt!");
-        linkObj = {
+        //add form info to link object
+        let newLink = {
             linkName: document.getElementById('link-name').value, 
             linkUrl: document.getElementById('link-url').value 
-            }
+        };
         
-        linkArray.push(linkObj);
+        //add new link to linkArray
+        linkArray.push(newLink);
+        //convert linkArray and save in localStorage
         localStorage.setItem('links', JSON.stringify(linkArray));
 
+        //update links shown on dashboard
+        renderLinks();
+
+        //hide modal and reset form
+        linkModal.classList.add('link-modal-hidden');
+        modalForm.reset();
+
     } else {
-        alert("Alla fält är inte korrekt ifyllda. \nKom du ihåg https:// i början av webbadressen? \nProva igen!");
+        alert("Alla fält är inte korrekt ifyllda.\n" + 
+            "Kom du ihåg https:// i början av webbadressen?\n" +
+            "Prova igen!"
+        );
     }
 })
 
-//------- Show add-links modal -------------
-addLinkBtn.addEventListener('click', function(){
-    linkModal.classList.remove('link-modal-hidden');
-});
+//------- FUNCTION Show add-links modal -------------
+addLinkBtn.addEventListener('click', () => linkModal.classList.remove('link-modal-hidden'));
 
-//------- Hide add-links modal -------------
-closeLinkModal.addEventListener('click', function(){
-    linkModal.classList.add('link-modal-hidden');
-})
+//------- FUNCTION Hide add-links modal -------------
+closeLinkModal.addEventListener('click', () => linkModal.classList.add('link-modal-hidden'));
 
-//------- Delete links -------------------------
-console.log(linkArray);
+//------- FUNCTION Delete links -------------------------
 function deleteLink(linkIndex) {
+    //remove link at specified index
     linkArray.splice(linkIndex, 1);
 
-    if(linkArray.length === 0) {
-        localStorage.removeItem('links');
-    } else {
-        localStorage.setItem('links', JSON.stringify(linkArray));
-    }
+    /* if linkArray is empty remove links from localStorage,
+    otherwise update the stored array */
+    linkArray.length === 0
+        ? localStorage.removeItem('links') 
+        : localStorage.setItem('links', JSON.stringify(linkArray));
+    
+    //call renderLinks to update links shown on dashboard
     renderLinks();
 }
