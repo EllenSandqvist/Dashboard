6f8d617c88076a3ebdfadfc0e35eaf4deaa3e9f7
Ellen Sandqvist <ell1sandqvist@gmail.com>
2023-12-26 17:23:20 +0100

    added more comments and renamned a function

--- js/h1-header.js
+++ js/h1-header.js
@@ -1,23 +1,38 @@
 const h1Header = document.querySelector('h1');
+
+/* store Dashboard-h1 data from localStorage if there is any in headerContent, 
+otherwise store h1 text content */
 let headerContent = localStorage.getItem('Dashboard-h1') || h1Header.textContent;
 
-//function to render heading
+//call function to render main heading
+renderHeading();
+
+
+//------------------------------------------------------------
+// Function to render heading
+//------------------------------------------------------------
 function renderHeading() {
     h1Header.textContent = headerContent;
 }
 
-//eventlistener on h1 that listens to input
+
+//--------------------------------------------------------------
+// Eventlistener on h1 with callback that updates headerContent
+//--------------------------------------------------------------
 h1Header.addEventListener('input', () => {
     headerContent = h1Header.textContent;
 
     //function for saving headercontent to localStorage
-    saveH1ToLocalStorage(headerContent);
+    saveHeaderToLocalStorage(headerContent);
 
+    //render heading with updated content
     renderHeading();
 });
 
-function saveH1ToLocalStorage(heading) {
+
+//--------------------------------------------------------------
+// Function to save H1 to localStorage
+//--------------------------------------------------------------
+function saveHeaderToLocalStorage(heading) {
     localStorage.setItem('Dashboard-h1', heading);
 }
-
-renderHeading();
